b0c340af2a6ce6c7e4e1c1ce86e57c9889ef5323
Danskuvv <danielvv@metropolia.fi>
2024-01-24 14:18:37 +0200

    graphql server created

---
+++ hybrid-graphql-server/src/api/resolvers/tagResolver.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import {fetchAllTags, fetchTagsByMediaId} from '../models/tagModel';
+
+export default {
+  MediaItem: {
+    tags: async (parent: {media_id: string}) => {
+        return await fetchTagsByMediaId(Number(parent.media_id));
+    },
+},
+    Query: {
+        tags: async () => {
+            return await fetchAllTags();
+        },
+    },
+};
