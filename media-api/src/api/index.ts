e178e089f0f90348f396c73f46077776af34cd98
Danskuvv <danielvv@metropolia.fi>
2024-02-25 04:57:29 +0200

    added comment functionality

--- media-api/src/api/index.ts
+++ media-api/src/api/index.ts
@@ -3,6 +3,7 @@ import express, {Request, Response} from 'express';
 import mediaRoute from './routes/mediaRoute';
 import tagRoute from './routes/tagRoute';
 import likeRoute from './routes/likeRoute';
+import commentRoute from './routes/commentRoute';
 
 const router = express.Router();
 
@@ -15,5 +16,6 @@ router.get('/', (req: Request, res: Response) => {
 router.use('/media', mediaRoute);
 router.use('/tags', tagRoute);
 router.use('/likes', likeRoute);
+router.use('/comments', commentRoute);
 
 export default router;
